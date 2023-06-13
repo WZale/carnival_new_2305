078a13774150fe1383cecbfe39fb73875f53713d
William Zale <williamzale@gmail.com>
2023-06-13 11:16:10 -0400

    Initial commit

---
+++ lib/visitor.rb
@@ -0,0 +1,21 @@
+class Visitor
+  attr_reader :name,
+              :height,
+              :preferences
+
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @raw_spending_money = spending_money
+    @preferences = []
+  end
+
+  def spending_money
+    @raw_spending_money.delete("$").to_i
+  end
+
+  def add_preference(preference)
+    @preferences << preference
+  end
+end
+
---
+++ spec/visitor_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper.rb'
+
+RSpec.describe Visitor do
+  before do
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+  end
+
+  describe "#exists" do
+    it "exists with readable attributes" do
+      expect(@visitor1).to be_an_instance_of(Visitor)
+      expect(@visitor1.name).to eq("Bruce")
+      expect(@visitor1.height).to eq(54)
+      expect(@visitor1.spending_money).to eq(10)
+      expect(@visitor1.preferences).to eq([])
+    end
+  end
+
+  describe "#add_preferences" do
+    it "can add a preference about a type of ride" do
+      @visitor1.add_preference(:gentle)
+      @visitor1.add_preference(:thrilling)
+
+      expect(@visitor1.preferences).to eq([:gentle, :thrilling])
+    end
+  end
+end
