9b3c76dbfd936981a8cb0ed991c5c5ddbdcfb908
William Zale <williamzale@gmail.com>
2023-06-13 13:01:21 -0400

    feat: Add initial class structure and methods for add_ride and most_popular_ride

---
+++ lib/carnival.rb
@@ -0,0 +1,22 @@
+class Carnival
+  attr_reader :name,
+              :duration,
+              :rides
+
+  def initialize(name, duration)
+    @name = name
+    @duration = duration
+    @rides = []
+  end
+
+  def add_ride(ride)
+    @rides << ride
+  end
+
+  def most_popular_ride
+    @rides.max_by do |ride|
+      ride.rider_log.length
+    end
+  end
+
+end
